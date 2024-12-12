361c12fceb32c1aa4b1be5a31e6384119b720b6b
AdamSkubida <adasiow.s@gmail.com>
2024-12-12 12:50:53 +0100

    Header feature/nav, logo, contact styiling  index.css update/global styles changed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,29 @@
+import { Link } from "react-router-dom";
 import css from "./Header.module.css";
 
 export const Header = () => {
   return (
     <>
-      <div className={css.home}>
+      <div className={css.header}>
         <nav className={css.navigation}>
-          <ul className={css.list}>
-            <li className={css.item}>O nas</li>
-            <li className={css.item}>Cennik</li>
+          <ul className={`${css.list} ${css["navigation-list"]}`}>
+            <li className={`${css.item} ${css["navigation-item"]}`}>O nas</li>
+            <li className={`${css.item} ${css["navigation-item"]}`}>Cennik</li>
           </ul>
         </nav>
+        <div className={css.logo}>
+          <Link to="/">
+            <h2>DeresDetailing</h2>
+          </Link>
+        </div>
+        <div className={css.contact}>
+          <ul className={`${css.list} ${css["contact-list"]}`}>
+            <li className={`${css.item} ${css["contact-item"]}`}>12343546</li>
+            <li className={`${css.item} ${css["contact-item"]}`}>
+              detailing@gmail.com
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
