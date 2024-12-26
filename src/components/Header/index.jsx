6f9088d1083ca631336cb9fc86af58e0846c9f38
AdamSkubida <adasiow.s@gmail.com>
2024-12-26 16:14:47 +0100

    Header bugfix/added wrapper fixing margins and position of header  -  Hero bugfix/video position changed, offer box position and media scaling fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,26 +5,30 @@ export const Header = () => {
   return (
     <>
       <div className={css.header}>
-        <nav className={css.navigation}>
-          <ul className={`${css.list} ${css["navigation-list"]}`}>
-            <li className={`${css.item} ${css["navigation-item"]}`}>O nas</li>
-            <li className={`${css.item} ${css["navigation-item"]}`}>Cennik</li>
-          </ul>
-        </nav>
-        <div className={css.logo}>
-          <Link to="/">
-            <h2>DeresDetailing</h2>
-          </Link>
-        </div>
-        <div className={css.contact}>
-          <ul className={`${css.list} ${css["contact-list"]}`}>
-            <li className={`${css.item} ${css["contact-item"]}`}>
-              +48 987 654 321
-            </li>
-            <li className={`${css.item} ${css["contact-item"]}`}>
-              detailing@gmail.com
-            </li>
-          </ul>
+        <div className={css.wrapper}>
+          <nav className={css.navigation}>
+            <ul className={`${css.list} ${css["navigation-list"]}`}>
+              <li className={`${css.item} ${css["navigation-item"]}`}>O nas</li>
+              <li className={`${css.item} ${css["navigation-item"]}`}>
+                Cennik
+              </li>
+            </ul>
+          </nav>
+          <div className={css.logo}>
+            <Link to="/">
+              <h2>DeresDetailing</h2>
+            </Link>
+          </div>
+          <div className={css.contact}>
+            <ul className={`${css.list} ${css["contact-list"]}`}>
+              <li className={`${css.item} ${css["contact-item"]}`}>
+                +48 987 654 321
+              </li>
+              <li className={`${css.item} ${css["contact-item"]}`}>
+                detailing@gmail.com
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
